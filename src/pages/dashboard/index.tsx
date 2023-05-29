6a2dc5e452357c3caf43ad4e23333f62e7498022
jhonata <jhoownogueira@outlook.com>
2023-05-29 09:55:21 -0300

    :art: Header Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import { DashboardContainer } from "@/styles/dashboard/styles";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Image from "next/image"
+import { Listbox } from '@headlessui/react'
+import { CaretDown, CaretLeft, CaretUp } from "@phosphor-icons/react";
 
 interface UserProps {
   id: number;
@@ -17,6 +19,16 @@ export default function Dashboard() {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<UserProps>();
   const router = useRouter();
+
+  const wallets = [
+    { id: 1, title: 'Carteira Jhonata'},
+    { id: 2, title: 'Carteira Familia'},
+    { id: 3, title: 'Carteira Poupança'},
+    { id: 4, title: 'Carteira Kiara' },
+  ]
+
+  const [selectedWallet, setSelectedWallet] = useState(wallets[0])
+
 
   useEffect(() => {
     const fetchAuthenticationStatus = async () => {
@@ -54,17 +66,29 @@ export default function Dashboard() {
                 className='profile-picture'
                 src={user?.picture ? user.picture : '/images/logo.png'}
                 alt='logo myFinance'
-                width={32}
-                height={32}
+                width={42}
+                height={42}
               />
             </div>
-            <select name="walltes" id="">
-              <option value="walltes">Carteira Jhonata</option>
-              <option value="walltes">Carteira Letícia</option>
-            </select>
+            <Listbox value={selectedWallet} onChange={setSelectedWallet}>
+              <Listbox.Button className="styled-select-button">
+                {selectedWallet.title}
+                <CaretLeft size={24} />
+                </Listbox.Button>
+              <Listbox.Options className="styled-select-options">
+                {wallets.map((wallet) => (
+                  <Listbox.Option
+                    key={wallet.id}
+                    value={wallet}
+                  >
+                    {wallet.title}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Listbox>
             <span>R$1.500,00</span>
           </div>
-          <button>Novo lançamento</button>
+          <button className="new-release">Novo lançamento</button>
         </header>
       </DashboardContainer>
     </>
--- src/styles/dashboard/styles.ts
+++ src/styles/dashboard/styles.ts
@@ -6,7 +6,107 @@ export const DashboardContainer = styled.section`
   display: flex;
   flex-direction: column;
 
-  h1 {
-    color: white;
+  header {
+    width: 100%;
+    height: 2.5rem;
+    margin-top: 2.6rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .left-content {
+      position: relative;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      .picture-content {
+        width: 2.625rem;
+        height: 2.625rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 999px;
+        border: 2px solid ${props => props.theme.secondary};
+        overflow: clip;
+
+        .profile-picture {
+          width: 2.625rem;
+          height: 2.625rem;
+          object-fit: cover;
+        }
+      }
+
+      .styled-select-button {
+        width: 280px;
+        display: flex;
+        padding: 0.5rem;
+        align-items: center;
+        justify-content: space-between;
+        background: transparent;
+        border: none;
+        cursor: pointer;
+        
+
+        &[aria-expanded="false"] {
+          svg {
+            transition: all 0.2s;
+            transform: rotate(0deg);
+          }
+        }
+
+        &[aria-expanded="true"] {
+          svg {
+            transition: all 0.2s;
+            transform: rotate(-90deg);
+          }
+        }
+      }
+
+      .styled-select-options {
+        position: absolute;
+        width: 280px;
+        left: 3.2rem;
+        top: 42px;
+        background: ${props => props.theme["black-soft"]};
+        border: 1px solid ${props => props.theme.hr};
+        display: flex;
+        flex-direction: column;
+        border-radius: 12px;
+        overflow: clip;
+
+        li {
+          padding: 0.8rem 1rem;
+          cursor: pointer;
+          &:hover {
+            background: ${props => props.theme.secondary};
+          }
+        }
+      }
+
+
+      span {
+        font-weight: 600;
+        font-size: 1.25rem;
+        line-height: 110%;
+        letter-spacing: -0.03em;
+      }
+    }
+
+    button.new-release {
+      height: 100%;
+      border-radius: 8px;
+      border: 0;
+      outline: none;
+      padding: 8px 16px;
+      font-size: 0.875rem;
+      color: ${props => props.theme["text-white"]};
+      background: ${props => props.theme.secondary};
+      transition: all 0.2s;
+
+      &:hover {
+        background: ${props => props.theme.primary};
+      }
+    }
   }
 `
